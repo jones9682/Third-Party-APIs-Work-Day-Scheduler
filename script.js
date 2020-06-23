01a60f83384081c14a119ade712253ed98b8f6c3
Bobby Jones Jr <jones9682@yahoo.com>
2020-06-22 23:05:56 -0400

    added save button function and local storage

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// moment.js
-$("#currentDay").text(moment().format("dddd, MMM Do YYYY,"));
+$(document).ready(function () {
+  // display current day, month and year, moment.js
+  $("#currentDay").text(moment().format("dddd, MMM Do YYYY,"));
+
+  // save button for the user input
+  $(".saveBtn").on("click", function () {
+    console.log(this);
+    // loads the user description in the value local storage
+    var text = $(this).siblings(".description").val();
+    // loads the hour in the key local storage
+    var time = $(this).parent().attr("id");
+
+    // puts description text in local storage.
+    localStorage.setItem(time, text);
+  });
+
+  $("#hr8 .description").val(localStorage.getItem("hr8"));
+});
